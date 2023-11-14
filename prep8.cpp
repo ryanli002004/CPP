a50947aec29e3d04ddb5f7594b16e2829179fe2c
ryanli002004 <ryanli002004@gmail.com>
2023-11-14 00:02:23 -0500

    c

--- prep8.cpp
+++ prep8.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
-
+long long doubleNum(long long x) {
+    if (x == 0) {
+        return 0;
+    }
+    return 100 * doubleNum(x / 10) + 10 * (x % 10) + (x % 10);
+}
 
 int main(){
+    cout<<doubleNum(248462)<<endl;
     return 0;
 }
